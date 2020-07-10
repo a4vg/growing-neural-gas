3f15a1c2c1b7eae0717b6d410dcc4f1bf977fdbf
Andrea Velasquez <andrea.velasquez@utec.edu.pe>
2020-07-10 17:06:45 -0500

    feat(Image): Draw graph to image

--- src/Image.h
+++ src/Image.h
@@ -2,6 +2,7 @@
 #define IMAGE_H
 
 #include <opencv2/opencv.hpp>
+#include "Graph.h"
 
 class Image
 {
@@ -15,10 +16,30 @@ class Image
   static int normalize(int px);
 
   public:
+    Image(cv::Mat _img): img(_img) {};
     Image(std::string imgpath): img(cv::imread(imgpath)) {};
 
     void grayscale(cv::Mat &imgGray);
     void sobel(cv::Mat &imgSobel, bool binary);
+
+    template <typename G>
+    void overlapGraph(cv::Mat &imgGraph, G& g);
 };
+
+template <typename G>
+void Image::overlapGraph(cv::Mat &imgGraph, G& g)
+{
+  const int thickness = 2;
+  const cv::Scalar colorRGB = cv::Scalar(0, 0, 255); // red
+  imgGraph = this->img.clone();
+
+  for (auto& node: g)
+    for (auto& edge: node.second->getEdges())
+    {
+      cv::Point start = cv::Point(edge->nodes[0]->x, edge->nodes[0]->y);
+      cv::Point end = cv::Point(edge->nodes[1]->x, edge->nodes[1]->y);
+      cv::line(imgGraph, start, end, colorRGB, thickness);
+    }
+}
 
 #endif
--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,7 @@
 
 #include <iostream>
 #include "Image.h"
+#include "Graph.h"
 
 int main( int argc, char** argv )
 {
@@ -11,14 +12,37 @@ int main( int argc, char** argv )
     return -1;
   }
 
+  // Set files
   const std::string filepath = argv[1];
   const std::string filename = filepath.substr(filepath.find_last_of("/\\") + 1);
   const std::string outdir = "../output";
 
+  // Apply sobel mask to image
   Image img(filepath);
   cv::Mat imgSobel;
   img.sobel(imgSobel, true);
   cv::imwrite(outdir + "/sobel-" + filename, imgSobel);
+  std::cout << "Sobel applied to image: " << outdir << "/sobel-" << filename << "\n";
+
+  // Draw a square with diagonals from a graph
+  Graph<int, int, int> g;
+
+  g.addNode(1, 1, 10, 10);
+  g.addNode(2, 2, 40, 10);
+  g.addNode(3, 3, 40, 40);
+  g.addNode(4, 4, 10, 40);
+  g.addEdge(1, 2, 0);
+  g.addEdge(2, 3, 0);
+  g.addEdge(3, 4, 0);
+  g.addEdge(4, 1, 0);
+  g.addEdge(1, 3, 0);
+  g.addEdge(2, 4, 0);
+
+  Image blackImg(cv::Mat::zeros(cv::Size(50,50), CV_8UC3));
+  cv::Mat imgGraph;
+  blackImg.overlapGraph(imgGraph, g);
+  cv::imwrite(outdir + "/graph.png", imgGraph);
+  std::cout << "Graph drawn (square with diagonals): " << outdir + "/graph.png\n";
 
   return 0;
 }
